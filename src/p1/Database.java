dec825d29983ee22bb6308439d6241543160bff6
Matias Pizarro <matias.pizarro.o96@gmail.com>
2018-10-22 19:58:37 -0300

    code formatted

--- src/p1/Database.java
+++ src/p1/Database.java
@@ -3,18 +3,19 @@ package p1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class Database {
     Path file;
 
-    public Database(Path p) {
-        file = p;
+    public Database(String path) {
+        this.file = Paths.get(path);
     }
 
     public void insertar(Nodo n) {
         try {
-            Files.write(file, (n.toString() + String.format("%n")).getBytes(), StandardOpenOption.APPEND);
+            Files.write(this.file, (n.toString() + String.format("%n")).getBytes(), StandardOpenOption.APPEND);
         } catch (IOException x) {
             System.err.println(x);
         }
--- src/p1/Testeo.java
+++ src/p1/Testeo.java
@@ -8,11 +8,8 @@ public class Testeo {
     public static void main(String[] args) {
         Producto prod = new Producto(1, 100, 200, 300);
         Consumidor c = new Consumidor(23, "19514213-0", 0);
-        Path p = Paths.get("C:\\Users\\javie\\Desktop\\dict.txt");
-        Database d = new Database(p);
+        Database d = new Database("./dict.txt");
         d.insertar(c);
         d.insertar(prod);
-        d.insertar(c);
-        System.out.println("Hello World!"); // Display the string.
     }
 }
