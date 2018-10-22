63a3f87fcb24f194b8169e73b166e3161ff237ea
Matias Pizarro <matias.pizarro.o96@gmail.com>
2018-10-22 18:06:05 -0300

    class Nodo

---
+++ src/p1/Nodo.java
@@ -0,0 +1,13 @@
+package p1;
+
+import java.util.Hashtable;
+
+public abstract class Nodo {
+
+    private Hashtable<String, String> strAttr;
+    private Hashtable<String, Integer> intAttr;
+
+    public Nodo() {
+
+    }
+}
