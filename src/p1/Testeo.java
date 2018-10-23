674658024e2e2261f75ce6497314f9980210e984
Matias Pizarro <matias.pizarro.o96@gmail.com>
2018-10-22 23:54:09 -0300

    not used imports were deleted

--- src/p1/Testeo.java
+++ src/p1/Testeo.java
@@ -1,9 +1,5 @@
 package p1;
 
-import javax.xml.crypto.Data;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Testeo {
     public static void main(String[] args) {
         Producto prod = new Producto(1, 100, 200, 300);
