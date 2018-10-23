81ced5892823569ecad6cd87d23969f831c1275a
Matias Pizarro <matias.pizarro.o96@gmail.com>
2018-10-23 18:50:11 -0300

    new class btree

---
+++ src/p2/Btree.java
@@ -0,0 +1,25 @@
+package p2;
+
+import p1.Nodo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Btree {
+
+    private int b;
+    private Nodo key;
+    private Btree[] children;
+
+    Btree(int b) {
+        this.b = b;
+        children = new Btree[b];
+    }
+
+    List<Nodo> search(String attr) {
+        List<Nodo> nlist = new ArrayList<>();
+
+        return n;
+    }
+
+}
