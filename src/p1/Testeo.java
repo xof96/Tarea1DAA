100751ac46efe9b1649e43de0c0839fa5da93022
Matias Pizarro <matias.pizarro.o96@gmail.com>
2018-10-23 16:29:47 -0300

    merged done

--- src/p1/Testeo.java
+++ src/p1/Testeo.java
@@ -4,11 +4,22 @@ import java.util.Random;
 
 public class Testeo {
     public static void main(String[] args) {
+
         Database d = new Database("./productochico.txt");
+        Database d1 = new Database("./consumidor.txt");
         Random r = new Random();
         d.ordenar("hola");
-        /*for (int i = 1000001; i <= 10000000; i++) {
-            d.insertar(new Producto(i, r.nextInt(9991) + 10, r.nextInt(9001) + 1000, r.nextInt(991) + 10));
-        }*/
+
+//        for (int i = 1; i <= 10000; i++) {
+//            d.insertar(new Producto(i, r.nextInt(9991) + 10, r.nextInt(9001) + 1000, r.nextInt(991) + 10));
+//        }
+//
+//        for (int i = 1; i <= 10000; i++) {
+//            d1.insertar(new Consumidor(i,
+//                    String.valueOf(r.nextInt(16000001) + 8000000) + "-" +
+//                            String.valueOf(r.nextInt(10)),
+//                    r.nextInt(9001) + 1000));
+//        }
+
     }
 }
