85c780f48f1c038e3578ce3203bcaa1a52e368ca
javie <javier.munoz.c@ing.uchile.cl>
2018-10-23 18:52:20 -0300

    Merge try
    Signed-off-by: javie <javier.munoz.c@ing.uchile.cl>

--- src/p1/Database.java
+++ src/p1/Database.java
@@ -32,26 +32,70 @@ class Database {
         /*Servira para separar en caso si el archivo es grande o no
         long size=file.toFile().length();
         System.out.println(size);*/
-        List<Nodo> l = new ArrayList<>();
+        Nodo[] l = new Nodo[1000];
 
         Charset charset = Charset.forName("US-ASCII");
         try (BufferedReader reader = Files.newBufferedReader(this.file, charset)) {
             String line;
-            while ((line = reader.readLine()) != null && l.size() <= 1000) {
-                line = line.replaceAll("\\s+", "");
+            int i = 0;
+            while ((line = reader.readLine()) != null && i != 1000) {
                 List<String> nodostr = Arrays.asList(line.split(","));
-                int id = Integer.parseInt(nodostr.get(0));
-                int precio = Integer.parseInt(nodostr.get(1));
-                int ptsNec = Integer.parseInt(nodostr.get(2));
-                int ptsRec = Integer.parseInt(nodostr.get(3));
+                String id = nodostr.get(0);
+                String precio = nodostr.get(1);
+                String ptsNec = nodostr.get(2);
+                String ptsRec = nodostr.get(3);
                 Producto p = new Producto(id, precio, ptsNec, ptsRec);
-                l.add(p);
+                l[i] = p;
+                i++;
             }
         } catch (IOException x) {
             System.err.format("IOException: %s%n", x);
         }
+        mergeSort(l, l.length, attr);
+        System.out.println(l[0].attr.get(attr));
 
-        //System.out.println(l.get(1));
+    }
 
+    public static void mergeSort(Nodo[] a, int n, String attr) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        Nodo[] l = new Nodo[mid];
+        Nodo[] r = new Nodo[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid, attr);
+        mergeSort(r, n - mid, attr);
+
+        merge(a, l, r, mid, n - mid, attr);
+    }
+
+    public static void merge(Nodo[] a, Nodo[] l, Nodo[] r, int left, int right, String attr) {
+
+        int i = 0, j = 0, k = 0;
+        System.out.println(l[i].attr.get(attr));
+        System.out.println(r[j].attr.get(attr));
+
+/*        while (i < left && j < right) {
+            System.out.println(l[i].attr.get(attr));
+            System.out.println(r[j].attr.get(attr));
+            if (l[i].attr.get(attr).compareTo(r[j].attr.get(attr)) < 0) {
+                a[k++] = l[i++];
+            } else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }*/
     }
 }
--- src/p1/Producto.java
+++ src/p1/Producto.java
@@ -2,18 +2,18 @@ package p1;
 
 public class Producto extends Nodo {
 
-    public Producto(int id, int precio, int ptsNec, int ptsRec) {
+    public Producto(String id, String precio, String ptsNec, String ptsRec) {
         super();
-        this.attr.put("id", String.valueOf(id));
-        this.attr.put("precio", String.valueOf(precio));
-        this.attr.put("ptsNec", String.valueOf(ptsNec));
-        this.attr.put("ptsRec", String.valueOf(ptsRec));
+        this.attr.put("id", id);
+        this.attr.put("precio", precio);
+        this.attr.put("ptsNec", ptsNec);
+        this.attr.put("ptsRec", ptsRec);
     }
 
     @Override
     public String toString() {
         //hay que tratar de quitarle el espacio despues de la coma
-        return this.attr.get("id") + ", " + this.attr.get("precio") + ", " +
-                this.attr.get("ptsNec") + ", " + this.attr.get("ptsRec");
+        return this.attr.get("id") + "," + this.attr.get("precio") + "," +
+                this.attr.get("ptsNec") + "," + this.attr.get("ptsRec");
     }
 }
