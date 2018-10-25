4a27743797ce4a5bcbaf863dc8a134417fb4ddec
Matias Pizarro <matias.pizarro.o96@gmail.com>
2018-10-25 20:33:47 -0300

    btree updated

--- src/p2/Btree.java
+++ src/p2/Btree.java
@@ -7,23 +7,30 @@ import java.util.List;
 
 public class Btree {
 
-    private int b;
-    private Nodo key;
+    private int nLimit;
+    private int cLimit;
+    private List<Nodo> keys;
     private String orderCriteria;
     private List<Btree> children;
 
     Btree(int b, String criteria) {
-        this.b = b;
+        this.nLimit = b / 2;
+        this.cLimit = b / 2 + 1;
+        this.keys = new ArrayList<>();
         this.orderCriteria = criteria;
         this.children = new ArrayList<>();
     }
 
-    public int getB() {
-        return this.b;
+    public int getNLimit() {
+        return this.nLimit;
     }
 
-    public Nodo getKey() {
-        return this.key;
+    public int getCLimit() {
+        return this.cLimit;
+    }
+
+    public List<Nodo> getKey() {
+        return this.keys;
     }
 
     public String getOrderCriteria() {
@@ -34,9 +41,28 @@ public class Btree {
         return this.children;
     }
 
-
     public void insert(Nodo n) {
-        if (this.children == null) {
+        if (this.children.isEmpty()) {
+            if (this.keys.isEmpty()) {
+                this.keys.add(n);
+            }
+            else {
+                int i;
+                for (i = 0; i <= this.keys.size(); i++) {
+                    int v = n.getAttr().get(this.orderCriteria);
+                    if(i < this.keys.size() && v <= this.keys.get(i).getAttr().get(this.orderCriteria)) {
+                        this.keys.add(i, n);
+                    }
+                    else {
+                        this.keys.add(n);
+                    }
+                }
+                if (i > this.nLimit) {
+                    this.reshape();
+                }
+            }
+        }
+        else {
 
         }
     }
@@ -44,8 +70,8 @@ public class Btree {
     List<Nodo> searchEqual(String key, int value) {
         List<Nodo> nlist = new ArrayList<>();
         if (this.children == null) {
-            for (int i = 0; i < children.size(); i++) {
-                Nodo ni = children.get(i).getKey();
+            for (int i = 0; i < children.length; i++) {
+                Nodo ni = children.[i].getKey();
                 if (ni.getAttr().get(key) == value) {
                     nlist.add(ni);
                 }
