509cc2b892bb303399b0a7d420815f3791dbdaf7
Sebastian Donoso <sebastian.a.donoso.b@gmail.com>
2018-10-30 22:04:30 -0300

    test

--- src/p1/FileCreatorTest.java
+++ src/p1/FileCreatorTest.java
@@ -1,16 +1,40 @@
 package p1;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Random;
 
 public class FileCreatorTest {
 
     public void createFilesProductor() {
         Path file = Paths.get("./times.txt");
-        //Files.write(file, (n.toString() + String.format("%n")).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-        Database[] datas=new Database[7];
-        for
+        Database[] datas = new Database[7];
+        Random r = new Random();
+
+        for (int i = 1; i <= 7; i++) {
+            System.out.println(i);
+            double inputs = Math.pow(10, i);
+            long startTime = System.currentTimeMillis();
+            Database dataInUse = datas[i - 1];
+            dataInUse = new Database("./producto-" + Integer.toString(i) + ".txt");
+            for (double j = 1; j <= inputs; j++) {
+                dataInUse.insertar(new Producto((int)j, r.nextInt(9991) + 10, r.nextInt(9001) + 1000, r.nextInt(991) + 10));
+
+            }
+
+            long stopTime = System.currentTimeMillis();
+            long elapsedTime = stopTime - startTime;
+            String nameTest = "Test 10 a la " + Integer.toString(i) + " with time: ";
+            try {
+                Files.write(file, (String.format(nameTest) + Long.toString(elapsedTime) + String.format("%n")).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
+
 }
--- src/p1/Testeo.java
+++ src/p1/Testeo.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
 public class Testeo {
     public static void main(String[] args) {
+        FileCreatorTest test1 = new FileCreatorTest();
+        test1.createFilesProductor();
 
-        Database d = new Database("./producto.txt");
+        /*Database d = new Database("./producto.txt");
         Database d1 = new Database("./consumidor.txt");
         Random r = new Random();
-        d.ordenar("ptsNec");
+        d.ordenar("ptsNec");*/
 
 //        for (int i = 1; i <= 10000; i++) {
 //            d.insertar(new Producto(i, r.nextInt(9991) + 10, r.nextInt(9001) + 1000, r.nextInt(991) + 10));
